189def19334aa04bfb9bec8470182365c7278496
Danyaell <danyaellmtzo17@gmail.com>
2022-11-29 13:51:15 -0600

    Finishing-project

---
+++ app6.js
@@ -0,0 +1,23 @@
+//funciones basicas de js
+function despedir(){
+    alert("Adios desde una funcion en el body");
+}
+
+function jugar(){
+    alert("vamos jugar lolcito uwu");
+}
+
+function comer(){
+    alert("comiendo taquitos");
+}
+
+//consola
+console.log("Hola desde la consola");
+
+console.log("Texto Random xd");
+console.log(14);
+console.log(14.14);
+console.log(1,63);
+console.log(1+13);
+console.log(true);
+console.log(false);
